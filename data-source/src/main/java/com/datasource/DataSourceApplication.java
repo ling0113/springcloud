929a0c3c95e2a59ca9acc9ed81b592e9e8942fff
lgr0113@126.com <l5854531>
2020-05-18 11:19:43 +0800

    多数据源

---
+++ data-source/src/main/java/com/datasource/DataSourceApplication.java
@@ -0,0 +1,16 @@
+package com.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 多数据源测试
+ */
+@SpringBootApplication
+public class DataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSourceApplication.class, args);
+    }
+
+}
